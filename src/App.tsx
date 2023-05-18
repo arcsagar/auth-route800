230a293d9c2fef150b9f2318e94d2346472dc5c8
Rutuparna18 <rutu1801.rks@gmail.com>
2023-05-18 08:54:20 +0530

    rutuparna first commit

--- src/App.tsx
+++ src/App.tsx
@@ -36,6 +36,7 @@ function App() {
        onAuthRequired={customeAuthHandler}>
      
      <Header />
+     <h1> rutuparna</h1>
       <Switch>
         <Route path='/' exact> 
           <Redirect to='/home'  />

562a972405f6b06d43529c492e621279a1281cbf
rudranipofali <rudraniupofali.me@kdkce.edu.in>
2023-05-19 19:58:36 +0530

    main-r: change in header

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App() {
       oktaAuth={oktaAuth} 
       restoreOriginalUri={restoreOriginalUri} 
        onAuthRequired={customeAuthHandler}>
-     <h1>Rudrani</h1>
+     
      <Header />
      <h1> kshitij</h1>
      <h1> rutuparna</h1>
